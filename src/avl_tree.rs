0f2ebd6b4cbfebb7019ece45658780f99ef4fe7e
Samuel Chase <samebchase@gmail.com>
2014-06-26 17:52:19 +0530

    Add more trivial tests

--- src/avl_tree.rs
+++ src/avl_tree.rs
@@ -1,3 +1,15 @@
+pub struct Node {
+    pub left_child: Option<Box<Node>>,
+    pub data: u32,
+    pub right_child: Option<Box<Node>>
+}
+
+impl Node {
+    pub fn data_to_str(&self) -> String {
+        format!("{}", (*self).data)
+    }
+}
+
 pub fn foo() -> String {
     String::from_str("world")
 }
--- src/test.rs
+++ src/test.rs
@@ -1,5 +1,7 @@
 extern crate tree;
 
+use tree::avl_tree::Node;
+
 #[test]
 fn test_sanity() {
     let x = 2;
@@ -9,4 +11,11 @@ fn test_sanity() {
 #[test]
 fn trivial_import_from_lib() {
     assert!(format!("hello {}", tree::avl_tree::foo()) == String::from_str("hello world"));
+}
+
+#[test]
+#[should_fail]
+fn node_data_test() {
+    let node = Node { left_child: None, data: 1984, right_child: None };
+    assert!(node.data_to_str() == String::from_str("1983"));
 }
